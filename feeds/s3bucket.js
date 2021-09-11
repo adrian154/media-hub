af06c1db9285c47b689a4f35d72a9da42f161f47
adrian154 <94568a@gmail.com>
2021-09-10 23:42:49 -0700

    fix bugs w/ s3feed

--- feeds/s3bucket.js
+++ feeds/s3bucket.js
@@ -1,13 +1,15 @@
 module.exports = class {
 
     constructor(s3, bucket) {
+        this.s3 = s3;
         this.bucket = bucket;
     }
 
     async get(after) {
-        return this.s3.listObjects(this.bucket, after).Contents.map(entry => ({
+        return (await this.s3.listObjects(this.bucket, after)).Contents.map(entry => ({
             type: "image",
-            url: `https://${this.bucket}.${this.s3.config.endpoint}/${entry.key}`
+            url: `https://${this.bucket}.${this.s3.config.endpoint}/${entry.Key}`,
+            id: entry.Key
         }));
     }
 
