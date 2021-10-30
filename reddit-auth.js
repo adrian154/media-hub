4a9bc328f450ed3e9968214afbf53583f6cbefa3
adrian154 <94568a@gmail.com>
2021-10-29 18:35:16 -0700

    make reddit auth more robust

--- reddit-auth.js
+++ reddit-auth.js
@@ -4,25 +4,37 @@ module.exports = class {
 
     constructor(user) {
         this.user = user;
-        this.refreshToken(user);
+        this.refreshToken();
     }
 
     async refreshToken() {
-        this.token = await this.getNewToken(this.user);
-        setTimeout(() => this.refreshToken(), (this.token.expires_in - 10) * 1000);
+        
+        if(!this.token || this.token.acquiredAt + this.token.expires_in * 1000 < Date.now()) {
+            return (this.token = await this.getNewToken());
+        }
+
+        return this.token;
+
     }
 
-    async getNewToken(user) {
-        return (await fetch(`https://www.reddit.com/api/v1/access_token?grant_type=password&username=${user.username}&password=${user.password}`, {
+    async getNewToken() {
+
+        const resp = await fetch(`https://www.reddit.com/api/v1/access_token?grant_type=password&username=${this.user.username}&password=${this.user.password}`, {
             method: "POST",
             headers: {
                 "User-Agent": "mediahub",
-                "Authorization": `Basic ${Buffer.from(user.clientID + ":" + user.clientSecret).toString("base64")}`
+                "Authorization": `Basic ${Buffer.from(this.user.clientID + ":" + this.user.clientSecret).toString("base64")}`
             }
-        })).json();
+        });
+
+        const token = await resp.json();
+        token.acquiredAt = Date.now();
+        return token;
+
     }
 
-    header() {
+    async header() {
+        await this.refreshToken();
         return this.token.token_type + " " + this.token.access_token;
     }
 
